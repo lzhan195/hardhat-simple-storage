8d17ab65ae3c1fdd59287a7a69c397c3d3562d7f
lzhan195 <longtengzhang77@gmail.com>
2023-06-09 23:51:28 -0400

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,42 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners()
+
+    for (const account of accounts) {
+        console.log(account.address)
+    }
+})
+
+const COINMARKEYCAP_API_KEY = ""
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
+    },
+    solidity: "0.8.8",
+    etherscan: { apikey: ETHERSCAN_API_KEY },
+    gasReporter: {
+        enabled: true,
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKEYCAP_API_KEY,
+    },
+}
